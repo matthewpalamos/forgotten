1b8e650334f3186d377754f027605d0b913c6bf1
Matt Palamos <mattpalamos@hotmail.com>
2017-09-11 16:12:40 -0700

    fixed merge changes

--- client/src/components/account/account.js
+++ client/src/components/account/account.js
@@ -39,6 +39,10 @@ class Account extends React.Component {
     this.backToMain = this.backToMain.bind(this);
     this.handleAudioClick = this.handleAudioClick.bind(this);
   }
+<<<<<<< HEAD
+=======
+
+>>>>>>> fixed merge changes
 
   componentWillMount () {
     Request.get('/userInfo', (data) => {
