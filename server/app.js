753908e8c2cba1d739115310a48547d1ae362745
Matt Palamos <mattpalamos@hotmail.com>
2017-09-18 15:23:54 -0700

    prepare rebase

--- server/app.js
+++ server/app.js
@@ -183,7 +183,6 @@ app.post('/initialItem', function(req, res) {
 
 
 app.post('/userItems', function (req, res) {
-  console.log(req.body);
   Items.fetchAll()
     .then((results) => {
       var change = results.map((item) => item.attributes).filter((item) => item.puzzle_id === req.user.level + 1);
@@ -201,7 +200,6 @@ app.post('/userItems', function (req, res) {
 });
 
 app.get('/puzzleItems', function (req, res) {
-  console.log(req.body);
   userItems.where({'user_id': req.user.id, equipped: 'yes'}).fetchAll()
     .then((results) => {
       var items = [];
